5b121ffcb01cc3244e0215b07bf537b69da3786c
Fujisaki <1838962+tatsuya-fujisaki@users.noreply.github.com>
2018-02-23 00:09:28 +0900

    Update clean.cmd

--- batch/clean/clean.cmd
+++ batch/clean/clean.cmd
@@ -4,6 +4,9 @@ echo off | clip
 
 javaws -uninstall
 
+rem Empty %TEMP%
+del /f /q /s %TEMP% > nul 2>&1
+
 rem Removing the backslash from C:\ invalidates "cd /d C:\".
 cd /d C:\
 del *.log /f /q /s > nul 2>&1
@@ -31,10 +34,10 @@ rd /q /s "%LOCALAPPDATA%\Microsoft\VSCommon" > nul 2>&1
 rd /q /s "%LOCALAPPDATA%\Microsoft\VsGraphics" > nul 2>&1
 rd /q /s "%LOCALAPPDATA%\Microsoft\WebsiteCache" > nul 2>&1
 
-rem Empty the temporary folder
-rd /q /s %TEMP% > nul 2>&1
-md %TEMP% > nul 2>&1
+rem Clear "Find" history in Visual Studio 2013
+reg delete HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\12.0\Find /f > nul 2>&1
 
+rem Clear "Find" history in Visual Studio 2015
 reg delete HKEY_CURRENT_USER\SOFTWARE\Microsoft\VisualStudio\14.0\Find /f > nul 2>&1
 
 rem Don't use "/sageset" as it changes registry.
