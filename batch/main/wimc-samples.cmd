8d82bcabdde5a015c5eabd7cf54641c5480cc4bc
Fujisaki <1838962+tatsuya-fujisaki@users.noreply.github.com>
2018-03-20 19:35:51 +0900

    Update wimc-samples.cmd

--- batch/main/wimc-samples.cmd
+++ batch/main/wimc-samples.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-wmic product list brief | sort > products.txt
-wmic service list brief | sort > services.txt
-wmic process list brief | sort > processes.txt
-wmic startup list brief | sort > startups.txt
+wmic /output:products.txt product list brief
+wmic /output:services.txt service list brief
+wmic /output:processes.txt process list brief
+wmic /output:startups.txt startup list brief
+wmic /output:updates.txt qfe list brief
 
 rem Set priority to the lowest
 wmic process where name="foo.exe" call setpriority idle
