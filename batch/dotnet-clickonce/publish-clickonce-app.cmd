a3f11f075fee4aa3dc4caa9486616a1a5ff7e2bb
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-04-14 21:45:09 +0900

    Add publish-clickonce-app.cmd

---
+++ batch/dotnet-clickonce/publish-clickonce-app.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+rem Place this batch file in a folder where a solution file exists or nuget throws an error.
+nuget restore
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsMSBuildCmd.bat"
+
+set AssemblyName=Assembly1
+set ClickOncePublishVersion=2020.1.1.0
+set ClickOnceProductName=Product1
+
+msbuild Project1\Project1.csproj "/p:Configuration=Release;AssemblyName=%AssemblyName%;ProductName=%ClickOnceProductName%;ApplicationVersion=%ClickOncePublishVersion%" /t:publish /v:m /m /nologo
+
+pause
