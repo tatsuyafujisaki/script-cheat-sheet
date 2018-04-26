a9d7e3f264efa328b4bd32eb543c2a4b0ecfe0a6
Fujisaki <1838962+tatsuya-fujisaki@users.noreply.github.com>
2018-04-26 21:03:42 +0900

    Update il.cmd

--- batch/dotnet-clickonce/il.cmd
+++ batch/dotnet-clickonce/il.cmd
@@ -1,8 +1,8 @@
 @echo off
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\LaunchDevCmd.bat"
+
 rem Output in HTML rather than GUI because the former is in color whereas the latter is in black and white.
 ildasm.exe /nobar /source /html /out=%~n1.html %1
 
 start %~n1.html
-
-rem https://docs.microsoft.com/en-us/dotnet/framework/tools/ildasm-exe-il-disassembler
