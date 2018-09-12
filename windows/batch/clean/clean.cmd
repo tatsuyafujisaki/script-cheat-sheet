bc1419681f9686359fc6ab00705c32548d51f027
Fujisaki <1838962+tatsuya-fujisaki@users.noreply.github.com>
2018-09-12 20:37:26 +0900

    Update clean.cmd

--- windows/batch/clean/clean.cmd
+++ windows/batch/clean/clean.cmd
@@ -7,34 +7,18 @@ del /f /q /s %TEMP% > nul 2>&1
 cd /d C:\
 del *.log /f /q /s > nul 2>&1
 
-rd /q /s "%USERPROFILE%\.oracle_jre_usage" > nul 2>&1
-rd /q /s "%USERPROFILE%\.thumbnails" > nul 2>&1
-rd /q /s "%USERPROFILE%\AppData\LocalLow\Sun" > nul 2>&1
-rd /q /s "%USERPROFILE%\Contacts"> nul 2>&1
-rd /q /s "%USERPROFILE%\Downloads" > nul 2>&1
-rd /q /s "%USERPROFILE%\Favorites"> nul 2>&1
-rd /q /s "%USERPROFILE%\Links" > nul 2>&1
-rd /q /s "%USERPROFILE%\Music" > nul 2>&1
-rd /q /s "%USERPROFILE%\OneDrive"> nul 2>&1
-rd /q /s "%USERPROFILE%\Pictures" > nul 2>&1
-rd /q /s "%USERPROFILE%\Saved Games"> nul 2>&1
-rd /q /s "%USERPROFILE%\Searches"> nul 2>&1
-rd /q /s "%USERPROFILE%\Videos"> nul 2>&1
-
-rem Delete Visual Studio files
-rd /q /s "%LOCALAPPDATA%\Microsoft\Phone Tools" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\Team Foundation" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\VisualStudio Services" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\VisualStudio\12.0\ComponentModelCache" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\VSCommon" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\VsGraphics" > nul 2>&1
-rd /q /s "%LOCALAPPDATA%\Microsoft\WebsiteCache" > nul 2>&1
-
 rem Clear "Find" history in Visual Studio 2015 and 2013
-reg delete HKCU\SOFTWARE\Microsoft\VisualStudio\14.0\Find /f > nul 2>&1
-reg delete HKCU\SOFTWARE\Microsoft\VisualStudio\12.0\Find /f > nul 2>&1
+rem reg delete HKCU\SOFTWARE\Microsoft\VisualStudio\14.0\Find /f > nul 2>&1
+rem reg delete HKCU\SOFTWARE\Microsoft\VisualStudio\12.0\Find /f > nul 2>&1
 
+rem There has been no programmatic way to clear "Find" history since Visual Studio 2017
+rem https://github.com/Microsoft/VSProjectSystem/blob/master/doc/overview/examine_registry.md
+rem https://visualstudio.uservoice.com/forums/121579-visual-studio-ide/suggestions/19961509-able-to-clear-find-replace-history-list
+
+rem Administrator privileges is NOT required to show admin-only items in sageset
 rem cleanmgr /sageset:0
+
+rem Administrator privileges is required to show admin-only items in sagerun
 cleanmgr /sagerun:0
 
 defrag /C /M /X
